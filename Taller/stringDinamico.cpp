7e64be794a9a1937b22ec0f1bdeb3f0d820a80bc
Miguel <miguelescanocedres@icloud.com>
2024-02-16 19:53:50 -0300

    modulo string
    modulo string

--- Taller/stringDinamico.cpp
+++ Taller/stringDinamico.cpp
@@ -6,3 +6,72 @@
 //
 
 #include "stringDinamico.hpp"
+
+
+void strcrear (string &s){
+    s = new char [1];
+    s[0]='\0';
+}
+
+void strdestruir (string &s){
+    delete []s;
+    s=NULL;
+}
+
+int strlar (string s){
+    int i=0;
+    while (s[i] != '\0')
+        i++;
+    return i;
+}
+
+void print (string s){
+    int i=0;
+    while(s[i]!= '\0'){
+        printf("%c", s[i]);
+        i++;
+    }
+}
+
+void strcop(string s, string &b){
+    delete[]b;
+    b=new char[strlar(s)+1];
+    int i=0;
+    while(s[i]!='\0'){
+        b[i]=s[i];
+        i++;
+    }
+    b[i]='\0';
+}
+
+void scan (string &s){
+    string aux= new char[MAX];
+    int i=0;
+    char c;
+    scanf ("%c", &c);
+    while ((c!='\n')&& (i<MAX-1)){
+        aux[i]=c;
+        i++;
+        scanf("%c",&c);
+    }
+    aux[i]='\0';
+    strcop(aux,s);
+    strdestruir(aux);
+}
+
+
+boolean streq (string s1, string s2)
+{
+    int i=0;
+    boolean iguales = TRUE;
+    while (iguales && (s1[i] != '\0') && (s2[i] != '\0')) {
+
+
+        if (s1[i] != s2[i])
+            iguales = FALSE;
+        i++;
+        if ((s1[i] != '\0') || (s2[i] != '\0'))
+            iguales = FALSE;
+    }
+        return iguales;
+    }
--- Taller/stringDinamico.hpp
+++ Taller/stringDinamico.hpp
@@ -16,8 +16,8 @@ void strcrear (string &s);
 void strdestruir (string &s);
 int strlar (string s);//Devuelve el largo del String
 void print (string s);//Despliega String por pantalla
+void strcop (string s, string &b); //Copia un string en otro string
 void scan (string &s);//Carga un String
 boolean streq (string s1, string s2);//reciba dos strings y devuelva TRUE si ambos strings son iguales o FALSE en caso contrario.
-void Bajar_String (string s, FILE * f);// Baja el string a un archivo.
-void Levantar_String (string &s, FILE * f);//Lecanta el string desde un archivo.
+
 #endif /* stringDinamico_hpp */
