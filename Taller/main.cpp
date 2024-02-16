852e1c7c33a19014a450c63c0a5dc15cca5b687d
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2024-02-16 11:30:16 -0300

    Initial Commit

---
+++ Taller/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Taller
+//
+//  Created by Gonzalo Fleitas on 16/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
