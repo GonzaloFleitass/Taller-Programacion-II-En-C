d813ca242730067a42c07289a57a956947e6c528
Matriller <83656418+Matriller@users.noreply.github.com>
2024-02-16 20:32:49 -0300

    listastring.hpp

--- Taller/listastring.hpp
+++ Taller/listastring.hpp
@@ -10,12 +10,16 @@
 
 #include <stdio.h>
 #include <stringDinamico.h>
+#include <boolean.h>
+
 typedef struct NodoL { 
      string s; 
      NodoL *sig; 
     } Nodo; 
 
- typedef Nodo * Lista;  
+ typedef Nodo * listastring;  
 
-
+void partirstring(string s, listastring &l);
+void insback (string s, listastring &l);
+ 
 #endif /* listastring.hpp */
