2f762d4a5b84754fe18b7592aaf9b493f64424b2
Matriller <83656418+Matriller@users.noreply.github.com>
2024-02-17 17:09:02 -0300

    Add files via upload

--- Taller/listastring.cpp
+++ Taller/listastring.cpp
@@ -9,11 +9,23 @@
 #include "listastring.hpp"
 
 void partirstring(string primero, listaString &l){
+    string aux;
+       strcrear (aux);
+
     if(esVacio(primero)==FALSE){
-        eliminarBlancosPrincipio(l->info, primero);
-        dividirString(l->info, primero, l->info);
+        while (l->sig!= NULL){
+            while (l->info != '\0'){
+        eliminarBlancosPrincipio(l->info, &primero);
+            dividirString(primero , &aux, &resto);
+                insback(separado, l ->info);
+            }
+
+            l = l->sig;
+        }
     }
 }
+
+
 void insback (string primero, listaString &l){
     listaString aux=l;
     while(aux->sig!=NULL){
