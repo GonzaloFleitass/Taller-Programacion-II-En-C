92df7179eacc5610ac6c58e6120b24118b616fa8
miguelescanocedres <114918783+miguelescanocedres@users.noreply.github.com>
2024-02-24 10:19:01 -0300

    Update expresion.hpp

--- taller 1/taller 1/expresion.hpp
+++ taller 1/taller 1/expresion.hpp
@@ -20,6 +20,6 @@ int darNum(Expresion a);
 AbbExp darexp(Expresion a, AbbExp b);
 void crearExpresion(Expresion);
 void MostrarExp(Expresion b);
-void cargaNumeroExpresion (Expresion a);
+void cargaPrimNumeroExpresion (Expresion a);
 
 #endif /* expresion_hpp */
