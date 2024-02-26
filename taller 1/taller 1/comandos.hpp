4a5501f8bffe7c2e7fb2f59751a09819365903df
Matriller <83656418+Matriller@users.noreply.github.com>
2024-02-26 20:33:10 -0300

    Update comandos.hpp

--- taller 1/taller 1/comandos.hpp
+++ taller 1/taller 1/comandos.hpp
@@ -14,7 +14,7 @@ void atomic(boolean a, Lista &b);
 void show (Lista l, int i);
 void compoundOrAnd (Lista &l, int i, int a, char c); 
 void compoundNot (Lista &l, int i, char c); 
-void evaluate(); 
+boolean evaluate(Lista l); 
 void save(); 
 void load(); 
 void exit (int &i);
