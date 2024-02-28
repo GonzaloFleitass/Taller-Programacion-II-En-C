4a2c8a98faf61464064fed129e8c776014b38a1c
Matriller <83656418+Matriller@users.noreply.github.com>
2024-02-28 17:34:12 -0300

    Update main.cpp

--- taller 1/taller 1/main.cpp
+++ taller 1/taller 1/main.cpp
@@ -12,41 +12,14 @@
 #include "listaString.hpp"
 int main() {
     
-    /*
-     string s,sb,separado,resto;
-     
-     listaString l;
-     Crear(l);
-     boolean a=TRUE;
-     boolean c=FALSE;
-     int n=2,z=1;
-     char w='a',v='n';
-     Lista b;
-     crearListaExp(b);
-     atomic(a, b);
-     atomic(c,b);
-     
-     compoundOrAnd(b,n,z,w);
-     // show(b,3);
-     // mostrarListaDeExp(b);
-     compoundNot (b,  n,  v);
-     mostrarBool(evaluateAbbs(darexp(ObtenerExpEnList(b,3))));
-     
-     
-     
-     // show(b,4);
-     }
-     
-     */
-    int salirprograma = 0;
-    
+    int salirprograma = 1;
     printf (" BOOLEAN CREATOR PRO ");
     printf(" - atomic: crea una nueva expresión booleana atómica (false o true) \n - compound: crea una nueva expresión booleana compuesta (contiene al menos un operador) \n - show: despliega por pantalla una expresión booleana previamente creada \n - evaluate: evalúa una expresión booleana, indicando si es verdadera o falsa \n - save: guarda en archivo una expresión booleana previamente creada  - load: recupera a memoria una expresión booleana previamente guardada en archivo  - exit: sale de la aplicación");
+  
     string s;
     listaString l,n;
     crearLista(n);
     cargarPalabras(n);
-    
     strcrear(s);
     crearLista(l);
     printf("\n");
@@ -54,14 +27,13 @@ int main() {
     printf("\n");
     scan(s);
     printf("\n");
- 
     partirString(s, l);
     mostrarlistastring(l);
-    printf("%c",devuelveComando(l,n));
-  
-}
-    /*
-    switch(devuleveComando(l){
+
+    
+ do {
+
+     switch(devuleveComando(l,n){
         a:  boolean c;
                 string t,f;
                 cargarTRUE(t);
@@ -74,17 +46,33 @@ int main() {
                     //cargar boolean en lista en caso de q sea false el boolean//
                 }
                 break;
+
+        c : 
+            
+            if (darstring(darPrimerLetra(l[2])) == ) {
+            
+            } else if ( not){
+            
+            }
+                 else{    
+                     printf("El compoun solo puede utilizarse con : and - or - not");
+             }
+                 break;
+
+        s:
+
+        e:
+
+        s:
+
+        l:
+
+        e: exit(salirprograma);
+             break;
         }
     }
-        
+     
+ } while ( salirprograma != 0);
 
-    
-   // do {}
-
-      //  while ( salirprograma != 0);
-
-    
     return 0;
 }
-
-*/
