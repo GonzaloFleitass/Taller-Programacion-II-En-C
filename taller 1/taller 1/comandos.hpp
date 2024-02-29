282decba41226935f71c002aa1eb947465796e63
Matriller <83656418+Matriller@users.noreply.github.com>
2024-02-29 09:07:46 -0300

    Update comandos.hpp

--- taller 1/taller 1/comandos.hpp
+++ taller 1/taller 1/comandos.hpp
@@ -10,13 +10,30 @@
 
 #include "listaString.hpp"
 #include "listaDeExpresiones.hpp"
+
+// Función para establecer un valor atómico en un nodo de la lista
 void atomic(boolean a, Lista &b);
+
+// Función para mostrar el contenido de la lista en la posición especificada
 void show (Lista l, int i);
+
+// Función para agregar una operación compuesta "OR" o "AND" en la lista en la posición especificada
 void compoundOrAnd (Lista &l, int i, int a, char c);
+
+// Función para agregar una operación compuesta "NOT" en la lista en la posición especificada
 void compoundNot (Lista &l, int i, char c); 
+
+// Función para evaluar el contenido de la lista a partir de la posición especificada
 void evaluate(Lista l ,int i); 
+
+// Función para guardar el estado de la lista en la posición especificada
 void save(Lista l, int i); 
+
+// Función para cargar arbol desde archivo
 void load(); 
+
+// Función para salir del programa y actualizar el valor de la variable entera i
 void exit (int &i);
+
 
 #endif /* comandos_hpp */
