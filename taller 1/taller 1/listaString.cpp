7d82de6c4f7de4090caab63f8b9010ead308f125
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2024-03-02 16:34:43 -0300

    k

--- taller 1/taller 1/listaString.cpp
+++ taller 1/taller 1/listaString.cpp
@@ -54,8 +54,9 @@ void partirString(string s, listaString &l){
 }
 
     
-void cargarPalabras(listaString &l){
-    string a,c,e,s,L,m,h;
+void cargarPalabras(listaString &l) {
+    string a, c, e, s, L, m, h;
+
     strcrear(a);
     strcrear(m);
     strcrear(c);
@@ -64,67 +65,68 @@ void cargarPalabras(listaString &l){
     strcrear(L);
     strcrear(h);
     
-    a[0]='a';
-    a[1]='t';
-    a[2]='o';
-    a[3]='m';
-    a[4]='i';
-    a[5]='c';
-    a[6]='\0';
-    insback(a,l);
-    
+    a[0] = 'a';
+    a[1] = 't';
+    a[2] = 'o';
+    a[3] = 'm';
+    a[4] = 'i';
+    a[5] = 'c';
+    a[6] = '\0';
+    insback(a, l);
 
-    c[0]='c';
-    c[1]='o';
-    c[2]='m';
-    c[3]='p';
-    c[4]='o';
-    c[5]='u';
-    c[6]='n';
-    c[7]='d';
-    c[8]='\0';
+    c[0] = 'c';
+    c[1] = 'o';
+    c[2] = 'm';
+    c[3] = 'p';
+    c[4] = 'o';
+    c[5] = 'u';
+    c[6] = 'n';
+    c[7] = 'd';
+    c[8] = '\0';
     insback(c, l);
-    
-    
-    h[0]='s';
-    h[1]='h';
-    h[2]='o';
-    h[3]='w';
-    h[4]='\0';
+
+    h[0] = 's';
+    h[1] = 'h';
+    h[2] = 'o';
+    h[3] = 'w';
+    h[4] = '\0';
     insback(h, l);
-    
-    e[0]='e';
-    e[1]='v';
-    e[2]='a';
-    e[3]='l';
-    e[4]='u';
-    e[5]='a';
-    e[6]='t';
-    e[7]='e';
-    e[8]='\0';
+
+    e[0] = 'e';
+    e[1] = 'v';
+    e[2] = 'a';
+    e[3] = 'l';
+    e[4] = 'u';
+    e[5] = 'a';
+    e[6] = 't';
+    e[7] = 'e';
+    e[8] = '\0';
     insback(e, l);
-    
-    s[0]='s';
-    s[1]='a';
-    s[2]='v';
-    s[3]='e';
-    s[4]='\0';
+
+    s[0] = 's';
+    s[1] = 'a';
+    s[2] = 'v';
+    s[3] = 'e';
+    s[4] = '\0';
     insback(s, l);
-    
-    L[0]='l';
-    L[1]='o';
-    L[2]='a';
-    L[3]='d';
-    L[4]='\0';
+
+    L[0] = 'l';
+    L[1] = 'o';
+    L[2] = 'a';
+    L[3] = 'd';
+    L[4] = '\0';
     insback(L, l);
-    
-    m[0]='e';
-    m[1]='x';
-    m[2]='i';
-    m[3]='t';
+
+    m[0] = 'e';
+    m[1] = 'x';
+    m[2] = 'i';
+    m[3] = 't';
+    m[4] = '\0';
     insback(m, l);
-    
+
+   
 }
+
 
 int conversorcai (string s){
     
