a1029920c88990428710f6bfd01c50f670e193f2
Matriller <83656418+Matriller@users.noreply.github.com>
2024-03-01 22:16:17 -0300

    Update boolean.hpp

--- taller 1/taller 1/boolean.hpp
+++ taller 1/taller 1/boolean.hpp
@@ -1,19 +1,11 @@
-//
-//  boolean.hpp
-//  taller 1
-//
-//  Created by Gonzalo Fleitas on 23/2/24.
-//
-
 #ifndef boolean_hpp
 #define boolean_hpp
-
 #include <stdio.h>
 
 typedef enum {FALSE,TRUE}boolean;
 
-// Función para mostrar un valor booleano por pantalla
 void mostrarBool(boolean a);
+bool verificar(string s);
 
                 
 #endif /* boolean_hpp */
