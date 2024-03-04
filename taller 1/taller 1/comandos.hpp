d5c303607d991d0f5fcd5e15a2620450dbc4b81a
Matriller <83656418+Matriller@users.noreply.github.com>
2024-03-04 20:10:20 -0300

    Update comandos.hpp

--- taller 1/taller 1/comandos.hpp
+++ taller 1/taller 1/comandos.hpp
@@ -1,29 +1,14 @@
 #ifndef comandos_hpp
 #define comandos_hpp
-
 #include "listaString.hpp"
 #include "listaDeExpresiones.hpp"
 
-// Función para establecer un valor atómico en un nodo de la lista
-void atomic(boolean a, Lista &b);
-
-// Función para mostrar el contenido de la lista en la posición especificada
-void show (Lista l, int i);
-
-// Función para agregar una operación compuesta "OR" o "AND" en la lista en la posición especificada
-void compoundOrAnd (Lista &l, int i, int a, char c);
-
-// Función para agregar una operación compuesta "NOT" en la lista en la posición especificada
-void compoundNot (Lista &l, int i, char c); 
-
-// Función para evaluar el contenido de la lista a partir de la posición especificada
-void evaluate(Lista l ,int i); 
-
-// Función para guardar el estado de la lista en la posición especificada
-void save(Lista l, int i);
-
-// Función para salir del programa y actualizar el valor de la variable entera i
-void exit (int &i);
-//Funcion para cargar una ex[resion previamente guardada.
-void loadArbol (AbbExp &a,Lista &l, string nombreArchivo);
+void atomic(boolean a, Lista &b);// Función para establecer un valor atómico en un nodo de la lista
+void show (Lista l, int i);// Función para mostrar el contenido de la lista en la posición especificada
+void compoundOrAnd (Lista &l, int i, int a, char c);// Función para agregar una operación compuesta "OR" o "AND" en la lista en la posición especificada
+void compoundNot (Lista &l, int i, char c); // Función para agregar una operación compuesta "NOT" en la lista en la posición especificada
+void evaluate(Lista l ,int i); // Función para evaluar el contenido de la lista a partir de la posición especificada
+void save(Lista l, int i);// Función para guardar el estado de la lista en la posición especificada
+void exit (int &i);// Función para salir del programa y actualizar el valor de la variable entera i
+void loadArbol (AbbExp &a,Lista &l, string nombreArchivo);//Funcion para cargar una ex[resion previamente guardada.
 #endif /* comandos_hpp */
