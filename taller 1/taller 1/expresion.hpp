e7036029262aac13b7aadb35bacf4c152e70f7e8
Matriller <83656418+Matriller@users.noreply.github.com>
2024-03-04 20:09:43 -0300

    Update expresion.hpp

--- taller 1/taller 1/expresion.hpp
+++ taller 1/taller 1/expresion.hpp
@@ -7,20 +7,10 @@ int num;
 AbbExp info;
 }Expresion;
 
-// Función para cargar una expresión en una estructura de expresión
-void cargarExpresion(Expresion &a, AbbExp b);
-
-// Función para obtener el número almacenado en una expresión
-int darNum(Expresion a);
-
-// Función para obtener la expresión almacenada en una estructura de expresión
-AbbExp darexp(Expresion a);
-
-// Función para mostrar una expresión por pantalla
-void MostrarExp(Expresion b);
-
-// Función para cargar un número en una estructura de expresión
-void cargaNumeroExpresion (Expresion &a, int n);
-
+void cargarExpresion(Expresion &a, AbbExp b);// Función para cargar una expresión en una estructura de expresión
+int darNum(Expresion a);// Función para obtener el número almacenado en una expresión
+AbbExp darexp(Expresion a);// Función para obtener la expresión almacenada en una estructura de expresión
+void MostrarExp(Expresion b);// Función para mostrar una expresión por pantalla
+void cargaNumeroExpresion (Expresion &a, int n);// Función para cargar un número en una estructura de expresión
 
 #endif
